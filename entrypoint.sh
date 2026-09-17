7b184ebf67bddc5f1b51cbfb16602083416d6f51
Nícolas Oliveira <nicolasfilipeolvr@gmail.com>
2026-09-17 19:34:31 -0300

    Add entrypoint script for botuser setup
    Create entrypoint.sh to set up data directory and run commands as botuser.

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+# Garante que /app/data pertence ao botuser, independente de como
+# o volume foi montado pelo host.
+mkdir -p /app/data
+chown -R botuser:botuser /app/data
+
+# Executa o comando como botuser
+exec gosu botuser "$@"
